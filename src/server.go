6a0ba32ab85e68da669267f589787f85aebbebc7
dak2 <dawt2h@gmail.com>
2023-07-23 17:31:02 +0900

    API疎通できるオリジンを限定してcredentialを受け取れるようにする

--- src/server.go
+++ src/server.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
 	"fmt"
+	"os"
 	"net/http"
 	router "shortcut_master_api/src/infrastructure"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/joho/godotenv"
 )
 
 func hello(c echo.Context) error {
@@ -13,13 +16,21 @@ func hello(c echo.Context) error {
 }
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("server start")
+
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		// TODO : set valid origin
-		AllowOrigins: []string{"*"},
+		AllowOrigins: []string{os.Getenv("ALLOWED_ORIGIN")},
 		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+		AllowCredentials: true,
 	}))
+
 	router.Handle(e)
 	e.Logger.Fatal(e.Start(":3000"))
 }
