de6855f54cbf4d2c9a1883d1ff2448a7092bf2d4
dak2 <dawt2h@gmail.com>
2023-11-12 17:53:13 +0900

    feat: insert answer histories

--- src/usecases/answer_history/answer_history_interactor.go
+++ src/usecases/answer_history/answer_history_interactor.go
@@ -16,10 +16,38 @@ func (interactor *AnswerHistoryInteractor) GetAnswerHistories(quizType string) (
 	return answerHistories, nil
 }
 
-func (interactor *AnswerHistoryInteractor) CreateAnswerHistories(answers []entity.AnswerHistory) (error) {
-	err := interactor.AnswerHistoryRepository.InsertAnswerHistories(answers)
+func (interactor *AnswerHistoryInteractor) CreateAnswerHistories(answers []entity.Answer, answerHistories []entity.AnswerHistoryUpdateRequest) error {
+
+	filterAnswerHistories := filterAnswerHistories(answers, answerHistories)
+	if filterAnswerHistories == nil {
+		return nil
+	}
+
+	err := interactor.AnswerHistoryRepository.InsertAnswerHistories(filterAnswerHistories)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func filterAnswerHistories(answers []entity.Answer, answerHistories []entity.AnswerHistoryUpdateRequest) []entity.AnswerHistory {
+	var finalAnswerHistories []entity.AnswerHistory
+
+	for _, answerHistory := range answerHistories {
+		isCorrect := false
+		for _, answer := range answers {
+			if answerHistory.AnswerId == answer.ID && answerHistory.Contents == answer.Contents {
+				isCorrect = true
+				break
+			}
+		}
+
+		finalAnswerHistories = append(finalAnswerHistories, entity.AnswerHistory{
+			AnswerId:  answerHistory.AnswerId,
+			Contents:  answerHistory.Contents,
+			IsCorrect: isCorrect,
+		})
+	}
+
+	return finalAnswerHistories
 }
